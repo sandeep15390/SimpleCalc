be1c69a6565728c7595ccc2f382250369ad70489
Sandeep Gurram <srgurram@cs.montana.edu>
2014-01-07 01:13:48 -0700

    US5 TA1 Bind the Result String to the text box

---
+++ CalculatorApplication/CalculatorViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleWpfApplication
+{
+    /// <summary>
+    /// ViewModel for the calculator
+    /// </summary>
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        public CalculatorViewModel()
+        {
+            Result = "some random text";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(String property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return _result;
+            }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        private string _result;
+    }
+}
