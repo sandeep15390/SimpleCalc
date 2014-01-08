b57c5e1f99aeb0ee015fad98eaed36c0284465e2
Sandeep Reddy Gurram <sandeep15390@gmail.com>
2014-01-08 11:58:38 -0700

    Update and rename MainWindow.xaml.cs to CalculatorView.xaml.cs

--- CalculatorApplication/CalculatorView.xaml.cs
+++ CalculatorApplication/CalculatorView.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace CalculatorApplication
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CalculatorView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CalculatorView : Window
     {
-        public MainWindow()
+        public CalculatorView()
         {
             InitializeComponent();
         }
