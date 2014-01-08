2076222b4f6af762e0cbc16dbce05e901e0aa895
Sandeep Gurram <srgurram@cs.montana.edu>
2014-01-08 13:18:29 -0700

    Rename the namespace appropriately

--- CalculatorApplication/CalculatorViewModel.cs
+++ CalculatorApplication/CalculatorViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SampleWpfApplication
+namespace WpfCalculatorApplication
 {
     /// <summary>
     /// ViewModel for the calculator
