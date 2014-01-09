b73803b1ab39c5098eeae66bc1429d0757de5fd3
Sandeep Gurram <srgurram@cs.montana.edu>
2014-01-09 00:53:48 -0700

    US1 TA4 Create Delegate command with parameter

--- CalculatorApplication/CalculatorViewModel.cs
+++ CalculatorApplication/CalculatorViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WpfCalculatorApplication
 {
@@ -17,6 +18,9 @@ namespace WpfCalculatorApplication
             Result = "some random text";
         }
 
+        /// <summary>
+        /// property changed handler
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void OnPropertyChanged(String property)
@@ -27,6 +31,57 @@ namespace WpfCalculatorApplication
             }
         }
 
+        /// <summary>
+        /// Command for number buttons
+        /// </summary>
+        public ICommand NumberButtonCommand
+        {
+            get
+            {
+                return new DelegateCommand(NumberButtonCommandExecuted);
+            }
+        }
+
+        private void NumberButtonCommandExecuted(object sender)
+        {
+            switch (int.Parse(sender.ToString()))
+            {
+                case 1:
+                    Result = "1";
+                    break;
+                case 2:
+                    Result = "2";
+                    break;
+                case 3:
+                    Result = "3";
+                    break;
+                case 4:
+                    Result = "4";
+                    break;
+                case 5:
+                    Result = "5";
+                    break;
+                case 6:
+                    Result = "6";
+                    break;
+                case 7:
+                    Result = "7";
+                    break;
+                case 8:
+                    Result = "8";
+                    break;
+                case 9:
+                    Result = "9";
+                    break;
+                case 0:
+                    Result = "0";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Result string property
+        /// </summary>
         public string Result
         {
             get
---
+++ CalculatorApplication/DelegateCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfCalculatorApplication
+{
+    /// <summary>
+    /// Implement DelegateCommand 
+    /// </summary>
+    public class DelegateCommand : ICommand
+    {
+        public DelegateCommand(Action<object> executeMethod)
+        {
+            _executeMethod = executeMethod;
+        }
+
+        private Action<object> _executeMethod;
+
+        /// <summary>
+        /// Returns if the comand can be executed 
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Can Execute changed event
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Execute the action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _executeMethod.Invoke(parameter);
+        }
+    }
+}
