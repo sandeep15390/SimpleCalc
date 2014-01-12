35fe151353b733510c6459fae00a3953959f20bd
Sandeep Gurram <srgurram@cs.montana.edu>
2014-01-12 15:19:16 -0700

    US11 TA6 Set up NUnit test framework with the solution

---
+++ CalculatorTests/CalculatorViewModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CalculatorTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            var integer1 = "23";
+            var integer2 = "23";
+            Assert.That(integer1.Equals(integer2));
+        }
+    }
+}
